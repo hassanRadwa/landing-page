61fba6cb6fd5a1a6b35eea8c5572e9ccc28fb9e3
radwaaly <radwaaly@users.noreply.github.com>
2020-07-20 01:07:03 +0200

    disable scroll with onclick
    and remove section ID from url when no section is selected

--- js/app.js
+++ js/app.js
@@ -18,7 +18,10 @@
  * 
 */
 let activeSectionId='';
+let secAncor = '';
+let pageUrl = window.location.href.split("#")[0];
 let sectionArray=[];
+let doScroll = 1;
 
 
 /**
@@ -27,7 +30,6 @@ let sectionArray=[];
  * 
 */
 function anchorOnclick(evt) {
-    
    activeSectionId = evt.target.getAttribute('href').split("#")[1];
    console.log(`anchorOnclick: activeSectionId: ${activeSectionId}`);
    // add circles effect
@@ -35,7 +37,9 @@ function anchorOnclick(evt) {
     const liElements=document.getElementsByTagName('li');
     //console.log(window.getComputedStyle(evt.target).background);
     //console.log(evt.target.getAttribute('style'));
-    
+    //window.removeEventListener('scroll',scrollHandler);
+    doScroll = 0;
+    const myDocFrag = document.createDocumentFragment();
     // change background on click and stay
     for (let liElement of liElements){
         if ( liElement.firstChild.getAttribute('href') === evt.target.getAttribute('href')){
@@ -45,6 +49,8 @@ function anchorOnclick(evt) {
             liElement.firstChild.className = 'menu__link';
         }
     }
+    console.log(`anchorOnclick: 1 doScroll: ${doScroll}`);
+    
     //console.log(window.href);
     //const hoverStyle = document.querySelector('.navbar__menu .menu__link'),':hover');
     //evt.target.style = hoverStyle;
@@ -53,7 +59,16 @@ function anchorOnclick(evt) {
     //this.className = "active";
     //evt.target.style.background = '#333';
 }
-
+function anchorOnclick2(evt) {
+   console.log(`anchorOnclick2: false activeSectionId: ${activeSectionId}`);
+//    doScroll = 1;
+   //window.addEventListener('scroll',scrollHandler);
+}
+function enablescroll()
+{
+    console.log(`in mouse up`);
+    window.addEventListener('scroll',scrollHandler);
+}
 function addCirclesEffect() {
     const sections = document.getElementsByTagName('section');
     for (let section of sections) {
@@ -65,77 +80,105 @@ function addCirclesEffect() {
         }
     }
 }
+
+function setDoScroll()
+{
+    console.log(`setDoScroll: 1 doScroll: ${doScroll}`);
+    doScroll = 1;
+    console.log(`setDoScroll: 2 doScroll: ${doScroll}`);
+}
+
 function scrollHandler(evt)
 {
-    let fromTop = Math.ceil(window.scrollY);
-    let secMargin = 0;
-    console.log(`scrollHandler: fromTop: ${fromTop}`);
-    const liElements=document.getElementsByTagName('li');
-    console.log(`scrollHandler: activeSectionId: ${activeSectionId}`);
-    if (activeSectionId == ''){
-        activeSectionId = 'section1';
-        const sectionsTmp = document.getElementsByTagName('section');
-        for (let sectionTmp of sectionsTmp){
-            secMargin = Math.floor(sectionTmp.offsetHeight * 0.5);
-            console.log(`sectionId: ${sectionTmp.id}`);
-            console.log(`scrollHandler: sectionTmp.offsetTop: ${sectionTmp.offsetTop}`);
-            console.log(`scrollHandler: sectionTmp_end: ${sectionTmp.offsetTop + sectionTmp.offsetHeight}`);
-            if (
-                sectionTmp.offsetTop - secMargin <= fromTop &&
-                (sectionTmp.offsetTop + sectionTmp.offsetHeight - secMargin) > fromTop
-              )
-              {
-                  activeSectionId = sectionTmp.id;
-                  console.log(`scrollHandler: activeSectionId: ${activeSectionId}`);
-                  break;
-              }
-        }
-    }
-    let currSection = document.getElementById(activeSectionId);
-    let currSecId=parseInt(activeSectionId.split('section')[1]);
-    secMargin = Math.floor(currSection.offsetHeight * 0.5);
-    console.log(`scrollHandler: currSecId: ${currSecId}`);
-    console.log(`scrollHandler: currSection.offsetTop: ${currSection.offsetTop}`);
-    console.log(`scrollHandler: currSection_end: ${currSection.offsetTop + currSection.offsetHeight - secMargin}`);
-    let newSecId;
-    if (
-        currSection.offsetTop - secMargin <= fromTop &&
-        currSection.offsetTop + currSection.offsetHeight - secMargin > fromTop
-      )
-      { // in the same section
-        liElements[currSecId-1].firstChild.className = 'active';
-        console.log(`scrollHandler: currSecId: ${currSecId}`);
+    if (doScroll == 1)
+    {
+        let fromTop = Math.ceil(window.scrollY);
+        let secMargin = 0;
+        console.log(`scrollHandler: fromTop: ${fromTop}`);
+        const liElements=document.getElementsByTagName('li');
         console.log(`scrollHandler: activeSectionId: ${activeSectionId}`);
-      }
-    else{ // section changed up or down
-        liElements[currSecId-1].firstChild.className = 'menu__link';
-        // get new section
-        if (fromTop < currSection.offsetTop - secMargin) { // scroll up
-            console.log('scrollHandler: scroll up');
-            newSecId = currSecId - 1;
-            if (newSecId - 1 >= 0) {
-                liElements[newSecId-1].firstChild.className = 'active';
-                activeSectionId = `section${newSecId}`;
-                console.log(`scrollHandler: newSecId: ${newSecId}`);
-                console.log(`scrollHandler: activeSectionId: ${activeSectionId}`);
+        //get active section id after reload
+        if (activeSectionId == ''){
+            // activeSectionId = 'section1';
+            const sectionsTmp = document.getElementsByTagName('section');
+            for (let sectionTmp of sectionsTmp){
+                secMargin = Math.floor(sectionTmp.offsetHeight * 0.5);
+                console.log(`sectionId: ${sectionTmp.id}`);
+                console.log(`scrollHandler: sectionTmp.offsetTop: ${sectionTmp.offsetTop}`);
+                console.log(`scrollHandler: sectionTmp_end: ${sectionTmp.offsetTop + sectionTmp.offsetHeight}`);
+                if (
+                    sectionTmp.offsetTop - secMargin <= fromTop &&
+                    (sectionTmp.offsetTop + sectionTmp.offsetHeight - secMargin) > fromTop
+                )
+                {
+                    activeSectionId = sectionTmp.id;
+                    secAncor = `#${activeSectionId}`;
+                    console.log(`scrollHandler: activeSectionId: ${activeSectionId}`);
+                    console.log(`scrollHandler: secAncor: ${secAncor}`);
+                    break;
+                }
             }
         }
-        else{ // scroll down
-            console.log('scrollHandler: scroll down');
-            newSecId = currSecId + 1;
-            if (newSecId - 1 < liElements.length) {
-                liElements[newSecId-1].firstChild.className = 'active';
-                activeSectionId = `section${newSecId}`;
-                console.log(`scrollHandler: newSecId: ${newSecId}`);
-                console.log(`scrollHandler: activeSectionId: ${activeSectionId}`);
-            }
+        if (activeSectionId != ''){
+        let currSection = document.getElementById(activeSectionId);
+        let currSecId=parseInt(activeSectionId.split('section')[1]);
+        secMargin = Math.floor(currSection.offsetHeight * 0.5);
+        console.log(`scrollHandler: currSecId: ${currSecId}`);
+        console.log(`scrollHandler: currSection.offsetTop: ${currSection.offsetTop}`);
+        console.log(`scrollHandler: currSection_end: ${currSection.offsetTop + currSection.offsetHeight - secMargin}`);
+        let newSecId;
+        if (
+            currSection.offsetTop - secMargin <= fromTop &&
+            currSection.offsetTop + currSection.offsetHeight - secMargin > fromTop
+        )
+        { // in the same section
+            liElements[currSecId-1].firstChild.className = 'active';
+            secAncor = `#${activeSectionId}`;
+            console.log(`scrollHandler: currSecId: ${currSecId}`);
+            console.log(`scrollHandler: activeSectionId: ${activeSectionId}`);
         }
+        else{ // section changed up or down
+            
+            // get new section
+            if (fromTop < currSection.offsetTop - secMargin) { // scroll up
+                console.log('scrollHandler: scroll up');
+                newSecId = currSecId - 1;
+                if (newSecId - 1 >= 0) {
+                    liElements[newSecId-1].firstChild.className = 'active';
+                    activeSectionId = `section${newSecId}`;
+                    secAncor = `#${activeSectionId}`;
+                    console.log(`scrollHandler: newSecId: ${newSecId}`);
+                    console.log(`scrollHandler: activeSectionId: ${activeSectionId}`);
+                }
+                else{ // reached the top of the page
+                    secAncor = '';
+                }
+            }
+            else{ // scroll down
+                console.log('scrollHandler: scroll down');
+                newSecId = currSecId + 1;
+                if (newSecId - 1 < liElements.length) {
+                    liElements[newSecId-1].firstChild.className = 'active';
+                    activeSectionId = `section${newSecId}`;
+                    secAncor = `#${activeSectionId}`;
+                    console.log(`scrollHandler: newSecId: ${newSecId}`);
+                    console.log(`scrollHandler: activeSectionId: ${activeSectionId}`);
+                }
+                else{ // reached the end of the page
+                    secAncor = '';
+                }
+            }
+            liElements[currSecId-1].firstChild.className = 'menu__link';
+        }
+        }
+        console.log(`----scrollHandler: activeSectionId: ${activeSectionId}`);
+        console.log(`----scrollHandler: secAncor: ${secAncor}`);
+        console.log(`----scrollHandler: pageUrl: ${pageUrl}`);
+        // change section ID in the URL
+        window.history.pushState("", "", `${pageUrl}${secAncor}`);
+        // add circles effect
+        addCirclesEffect();
     }
-    console.log(`----scrollHandler: activeSectionId: ${activeSectionId}`);
-    // change section ID in the URL
-    window.history.pushState("", "", `#${activeSectionId}`);
-    // add circles effect
-    addCirclesEffect();
 
     
 }
@@ -171,10 +214,15 @@ if (navList.childNodes.length === 0)
     }
     
     navList.appendChild(myDocFrag);
-    navList.addEventListener("click",anchorOnclick);
+    navList.addEventListener("click",anchorOnclick,true);
+    //navList.addEventListener("click",anchorOnclick2,false);
+    //navList.addEventListener('mousedown',enablescroll);
     //navList.addEventListener('mouseover',setActive);
     //const mainTag=document.getElementsByTagName('main')[0];
     window.addEventListener('scroll',scrollHandler);
+    window.addEventListener('wheel',setDoScroll);
+    window.addEventListener('keypress',setDoScroll);
+    window.addEventListener("mousedown", setDoScroll);
 }
 // Add class 'active' to section when near top of viewport
 
